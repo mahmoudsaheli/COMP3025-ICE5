33d1402768b28c238d3928205d9c2eff39a6823e
mahmoudsaheli <mahmoud_alsahily@hotmail.com>
2025-06-06 14:02:21 -0400

    first commit

---
+++ app/src/main/java/ca/georgiancollege/ice5/Calculator.kt
@@ -0,0 +1,16 @@
+package ca.georgiancollege.ice5
+
+import android.widget.Button
+import ca.georgiancollege.ice5.databinding.ActivityMainBinding
+
+
+class Calculator(private val binding: ActivityMainBinding) {
+
+
+
+
+
+
+
+
+}
